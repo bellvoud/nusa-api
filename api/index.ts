37d78d9142c41354c905af6a2d0583eaec521687
Bellvoud <hzr.hazardnfz@gmail.com>
2026-03-26 13:56:19 +0700

    fix : error 404

---
+++ api/index.ts
@@ -0,0 +1,9 @@
+// api/index.ts
+import app from "../src/index";
+
+export const GET = app.handle;
+export const POST = app.handle;
+export const PUT = app.handle;
+export const PATCH = app.handle;
+export const DELETE = app.handle;
+export const OPTIONS = app.handle;
