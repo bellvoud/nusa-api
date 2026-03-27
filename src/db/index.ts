6243ce94faafabf2e443f7f7e45f207e65d7be37
Bellvoud <hzr.hazardnfz@gmail.com>
2026-03-27 10:34:53 +0700

    fix : db

--- src/db/index.ts
+++ src/db/index.ts
@@ -4,7 +4,7 @@ import * as schema from "./schema";
 
 const client = postgres(process.env.DATABASE_URL!, {
   prepare: false,
-  max: 10, // max connections
+  max: 1,
 });
 
 export const db = drizzle(client, { schema });
